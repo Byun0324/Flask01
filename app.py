5e1a1261eb406ccf2e9c0ed9e7f9fc2b6863f2dd
Byun0324 <jshs2521@onedu.jje.go.kr>
2023-09-01 09:21:38 +0900

    new update~!

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, world!'
+
+@app.route('/apple')
+def page():
+    return 'Hello, 사과!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=80)
