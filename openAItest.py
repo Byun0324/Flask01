1045154923fec02c4bf583cdeb46340c81de6fb6
Byun0324 <jshs2521@onedu.jje.go.kr>
2023-09-08 09:21:28 +0900

    new update

--- openAItest.py
+++ openAItest.py
@@ -1,11 +1,28 @@
 import openai
-openai.api_key='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'
+from urllib.request import urlopen
+from datetime import datetime
+
+today = date.today().strftime("%Y/%m/%d")
+html = urlopen(f"https://jeju-s.jje.hs.kr/jeju-s/food/{today}/lunch")
+soup = BeautifulSoup(html, "html.parser")
+lunchmenu=soup.select("body > form > div > div > ul > li:nth-child(2) > dl > dd")[0].text
+
+openai.api_key = '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'
 
 messages = []
-messages.append({"role":"user", "content": "너의 이름은 춘식이고 제주과학고 1학년이야"})
+messages.append({"role":"system", "content": "너는 제주과학고 1학년이야"})
+messages.append({"role":"system", "content": "너의 이름은 춘식이야"})
+messages.append({"role":"system", "content": lunchmenu})
+
+messages.append({"role":"system", "content": "오늘 점심 메뉴는 매운갈비찜과 수제비국이야"})
+
+
+# messages.append({"role":"system", "content": ""})
+# messages.append({"role":"user", "content": ""})
+# messages.append({"role":"assistatnt", "content": ""})
 
 while True:
-    question=input("q: ")
+    question=input("q : ")
     if question == "": break
     messages.append({"role":"user", "content": question})
 
